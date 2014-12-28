08b3ae66c6a35bea543b6bb586cfde8d7b7a23b9
TomazWang <TomazWang.tw@gmal.com>
2014-12-28 07:42:46 -0500

    begin hacker1.credit

---
+++ C/ps1/credit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+
+/**
+  * BAD Credit from hacker1
+  */
+int main(void){
+
+    prinf("Number:");
+    long long numInput = getLongLong();
+    
+        
+    
+
+
+    // run successfully
+    return 0;
+}
