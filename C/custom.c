df9fa9ab0813656fb971308a48410feb36befdbb
Tomaz Wang <tomazwang.tw@gmail.com>
2014-12-30 12:21:35 +0000

    del unneeded files

--- C/custom.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "cs50.c"
-
-int main(void){
-    string userName;
-    printf("What's your name? \n");
-    userName = GetString();
-    printf("hello, %s\n", userName);
-}
